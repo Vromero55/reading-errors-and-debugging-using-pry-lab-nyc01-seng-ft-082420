9786ba6cd9dc11f991bf1d11b7a1efe3038857ca
Victor Romero <romeroa.victor@yahoo.com>
2020-08-09 15:24:05 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,15 @@
-# don't forget to add: require 'pry'
-
+require 'pry' # don't forget to add: require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
+  if string[0] = "s"
+    counter = 0
+    newword= string
+    while (counter < 10) do
+      newword[counter] = newword[0] + newword[counter]
+    counter += 1
+  #binding.pry
+  end
+newword
+else
     string
   end
 end
