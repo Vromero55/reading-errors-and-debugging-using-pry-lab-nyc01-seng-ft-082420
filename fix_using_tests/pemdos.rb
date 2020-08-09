4fd12e364db14568bbf27ea75c765491687606f2
Victor Romero <romeroa.victor@yahoo.com>
2020-08-09 15:16:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,8 +3,8 @@ def snake_it_up(string)
   if string[0] = "s"
     counter = 0
     newword= string
-    while (counter < 10) do
-      newword[counter] = newword[0] + newword[counter + 1]
+    while (counter < 11) do
+      newword[counter] = newword[0] + newword[counter]
     counter += 1
   #binding.pry
   end
