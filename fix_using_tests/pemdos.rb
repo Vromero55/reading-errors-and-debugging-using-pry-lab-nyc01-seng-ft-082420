475ce005c803993efddcc21366943bdbdfba32d8
Victor Romero <romeroa.victor@yahoo.com>
2020-08-09 14:54:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,13 @@
-# don't forget to add: require 'pry'
+require 'pry' # don't forget to add: require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
-  else
+    counter = 0
+    while (counter <= string.length) do
+      string[counter] = string[counter] * 10
+    counter +=1
+  end
+else
     string
   end
 end
