7327f938faba820c722d3f7b55cd81e0f0f3a3ea
Victor Romero <romeroa.victor@yahoo.com>
2020-08-09 15:12:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,6 +2,7 @@ require 'pry' # don't forget to add: require 'pry'
 def snake_it_up(string)
   if string[0] = "s"
     counter = 0
+    string = newword
     while (counter < 11) do
       string[counter] = string[0] + string[counter + 1]
     counter += 1
